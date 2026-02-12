35b2e8a2b160cec79f9058468b8f3a80f62039d3
Antoni Borowski <antekborowski11@gmail.com>
2026-02-13 00:07:08 +0100

    Resigned from commands for mvp and decided to use simple loop

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,17 @@
-use clap::{Parser, Subcommand};
-use std::path::PathBuf;
-
-use crate::explorer::Explorer;
+use std::{
+    io::{self, Read},
+    path::PathBuf,
+};
 
 mod explorer;
 
-#[derive(Parser)]
-struct Args {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    Go { path: PathBuf },
-    List,
-}
-
 fn main() {
-    let args = Args::parse();
-    let mut exporer = Explorer {
-        path: PathBuf::from("."),
-    };
-    match args.command {
-        Commands::Go { path } => {
-            exporer.set_path(path);
-        }
-        Commands::List => {
-            let list = exporer.list_dict();
-            for file in list {
-                print!("{file} ");
-            }
-        }
+    let mut input = String::new();
+    loop {
+        input.clear();
+
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+        let parts: Vec<&str> = input.split_whitespace().collect();
     }
 }
