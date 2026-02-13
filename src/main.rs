1b0e931a70b332a707efdb4bb23e3d55402e775f
Antoni Borowski <antekborowski11@gmail.com>
2026-02-13 19:06:05 +0100

    Replaced default io with rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,72 @@
-use std::{
-    io::{self, Read},
-    path::PathBuf,
-};
+use rustyline::hint::HistoryHinter;
+use rustyline::{completion::FilenameCompleter, CompletionType, Config, Editor};
+use rustyline_derive::{Completer, Helper, Highlighter, Hinter, Validator};
+use std::{env, fs, path::PathBuf};
 
 mod explorer;
 
-fn main() {
-    let mut input = String::new();
-    loop {
-        input.clear();
+#[derive(Completer, Helper, Hinter, Highlighter, Validator)]
+struct Helper {
+    #[rustyline(Completer)]
+    completer: FilenameCompleter,
+    #[rustyline(Hinter)]
+    hinter: HistoryHinter,
+}
 
-        io::stdin().read_line(&mut input).unwrap();
-        let input = input.trim();
+fn main() {
+    let config = Config::builder()
+        .completion_type(CompletionType::List)
+        .build();
+
+    let helper = Helper {
+        completer: FilenameCompleter::new(),
+        hinter: HistoryHinter::new(),
+    };
+    let mut rl = Editor::with_config(config).unwrap();
+    rl.set_helper(Some(helper));
+
+    let mut input = String::new();
+    let mut explorer_ = explorer::Explorer {
+        path: PathBuf::from("."),
+    };
+    loop {
+        let prompt = format!("{} ", env::current_dir().unwrap().display());
+
+        let readline = match rl.readline(&prompt) {
+            Ok(line) => line,
+            Err(_) => break,
+        };
+        let input = readline.trim().to_string();
+
+        rl.add_history_entry(&input).unwrap();
+
         let parts: Vec<&str> = input.split_whitespace().collect();
+        if parts.is_empty() {
+            continue;
+        }
+
+        let command = parts[0];
+        match command {
+            "go" => {
+                env::set_current_dir(parts[1]).unwrap();
+                explorer_.set_path(PathBuf::from(parts[1]));
+            }
+            "list" => {
+                let dict_files = explorer_.list_dict();
+                for file in dict_files {
+                    print!("{file} ");
+                }
+                println!();
+            }
+            "move" => {
+                fs::rename(parts[1], parts[2]).unwrap();
+            }
+            "quit" => {
+                break;
+            }
+            _ => {
+                println!("Undefinded command!");
+            }
+        }
     }
 }
